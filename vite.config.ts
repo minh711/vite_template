46493523780a90d0741a8a71cb8324411ed7d6f9
Trương Duy Minh <minhtd.developer@gmail.com>
2025-02-05 10:43:56 +0700

    build: add prettier and config eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), ""); // Load .env variables
+  const env = loadEnv(mode, process.cwd(), ''); // Load .env variables
 
   return {
     server: {
